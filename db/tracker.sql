cb6927da27a636d9beb1ba9de3ac007ddfaebad1
Gregor Cox <gregorcox@gmail.com>
2018-06-16 17:12:26 +0100

    creates merchants table

--- db/tracker.sql
+++ db/tracker.sql
@@ -2,7 +2,12 @@ DROP TABLE transactions;
 DROP TABLE tags;
 DROP TABLE merchants;
 
-CREATE table tags(
+CREATE TABLE tags(
+  id SERIAL4 primary key,
+  name VARCHAR(255)
+);
+
+CREATE TABLE merchants(
   id SERIAL4 primary key,
   name VARCHAR(255)
 );
