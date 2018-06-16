91e8a2a6c124cddc6b5f1682340cfb03d0f967fd
Gregor Cox <gregorcox@gmail.com>
2018-06-16 17:17:37 +0100

    creates transactions table

--- db/tracker.sql
+++ db/tracker.sql
@@ -11,3 +11,10 @@ CREATE TABLE merchants(
   id SERIAL4 primary key,
   name VARCHAR(255)
 );
+
+CREATE TABLE transactions(
+  id SERIAL4 primary key,
+  merchant_id INT4 REFERENCES merchants(id) ON DELETE CASCADE,
+  tag_id INT4 REFERENCES tags(id) ON DELETE CASCADE,
+  amount INT4
+);
