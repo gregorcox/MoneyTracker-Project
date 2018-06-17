2ec6439de4f98b448458ecf4e1fe13148e91fc1d
Gregor Cox <gregorcox@gmail.com>
2018-06-17 15:31:07 +0100

    fixes initialize error typo

--- models/tags.rb
+++ models/tags.rb
@@ -5,7 +5,7 @@ class Tag
   attr_reader(:name, :id)
 
   def initialize(options)
-    @id = options['id']to_i if options['id']
+    @id = options['id'].to_i if options['id']
     @name = options['name']
   end
 
--- models/transactions.rb
+++ models/transactions.rb
@@ -5,7 +5,7 @@ class Transaction
   attr_reader(:merchant_id, :tag_id, :amount, :id)
 
   def initialize(options)
-    @id = options['id']to_i if options['id']
+    @id = options['id'].to_i if options['id']
     @merchant_id = options['merchant_id'].to_i
     @tag_id = options['tag_id'].to_i
     @amount = options['amount'].to_i
