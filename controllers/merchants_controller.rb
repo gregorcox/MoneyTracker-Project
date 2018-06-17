15a3532ad3c03d595cf238ad26e721bbea20823c
Gregor Cox <gregorcox@gmail.com>
2018-06-17 18:56:07 +0100

    adds get merchant route

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -2,3 +2,8 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/merchants.rb' )
 also_reload( '../models/*' )
+
+get '/merchants' do
+  @merchants = Merchant.all()
+  erb ( :"merchants/index" )
+end
