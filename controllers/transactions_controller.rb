945169e3681db718f633b644e8b1b3ee87878116
Gregor Cox <gregorcox@gmail.com>
2018-06-18 10:05:52 +0100

    adds transaction get route

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -4,3 +4,8 @@ require_relative( '../models/transactions.rb' )
 require_relative( '../models/merchants.rb' )
 require_relative( '../models/tags.rb' )
 also_reload( '../models/*' )
+
+get '/transactions' do
+  @transactions = Transaction.all()
+  erb ( :"transactions/index" )
+end
