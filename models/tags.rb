78bd07e4bb367e3601a88467621a3cb92e5bf60d
Gregor Cox <gregorcox@gmail.com>
2018-06-18 10:59:09 +0100

    adds find method

--- models/tags.rb
+++ models/tags.rb
@@ -27,6 +27,14 @@ class Tag
     return result
   end
 
+  def self.find( id )
+    sql = "SELECT * FROM tags
+    WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run( sql, values )
+    return Tag.new( results.first )
+  end
+
   def delete()
     sql = "DELETE FROM tags
     WHERE id = $1"
