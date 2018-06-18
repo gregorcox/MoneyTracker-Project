8ce9c23631d7858d6953e9be69206e45c42c7eed
Gregor Cox <gregorcox@gmail.com>
2018-06-18 11:12:33 +0100

    adds a new route for adding a transaction

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -5,7 +5,15 @@ require_relative( '../models/merchants.rb' )
 require_relative( '../models/tags.rb' )
 also_reload( '../models/*' )
 
+#INDEX
 get '/transactions' do
   @transactions = Transaction.all()
   erb ( :"transactions/index" )
+end
+
+#NEW
+get '/transactions/new' do
+  @tags = Tag.all()
+  @merchants = Merchant.all()
+  erb ( :"transactions/new" )
 end
