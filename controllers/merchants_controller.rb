29a7aab10c4e30b53cb6de98492951f19f785e5c
Gregor Cox <gregorcox@gmail.com>
2018-06-18 10:51:07 +0100

    adds show route

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -3,7 +3,15 @@ require( 'sinatra/contrib/all' )
 require_relative( '../models/merchants.rb' )
 also_reload( '../models/*' )
 
+
+#INDEX
 get '/merchants' do
   @merchants = Merchant.all()
   erb ( :"merchants/index" )
+end
+
+#SHOW
+get '/merchants/:id' do
+  @merchant = Merchant.find(params['id'].to_i)
+  erb(:"merchants/show")
 end
