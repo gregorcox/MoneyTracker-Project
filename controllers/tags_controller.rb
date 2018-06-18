52d8be25ee75e9233b6f25d0c9c577156b3c5e2e
Gregor Cox <gregorcox@gmail.com>
2018-06-18 11:00:09 +0100

    adds route to show tag data

--- controllers/tags_controller.rb
+++ controllers/tags_controller.rb
@@ -3,7 +3,14 @@ require( 'sinatra/contrib/all' )
 require_relative( '../models/tags' )
 also_reload( '../models/*' )
 
+#INDEX
 get '/tags' do
   @tags = Tag.all()
   erb ( :"tags/index" )
+end
+
+#SHOW
+get '/tags/:id' do
+  @tag = Tag.find(params['id'].to_i)
+  erb(:"tags/show")
 end
