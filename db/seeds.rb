3041cb39b5106c5669e2c8af85c36df9f9f35dc4
Gregor Cox <gregorcox@gmail.com>
2018-06-19 12:12:01 +0100

    adds date input

--- db/seeds.rb
+++ db/seeds.rb
@@ -75,42 +75,42 @@ merchant5.save()
 transaction1 = Transaction.new({
   "merchant_id" => merchant1.id,
   "tag_id" => tag1.id,
-  "amount" => 2452
+  "amount" => 24.52
 })
 transaction1.save()
 
 transaction2 = Transaction.new({
   "merchant_id" => merchant3.id,
   "tag_id" => tag5.id,
-  "amount" => 1200
+  "amount" => 12.00
 })
 transaction2.save()
 
 transaction3 = Transaction.new({
   "merchant_id" => merchant4.id,
   "tag_id" => tag6.id,
-  "amount" => 799
+  "amount" => 7.99
 })
 transaction3.save()
 
 transaction4 = Transaction.new({
   "merchant_id" => merchant5.id,
   "tag_id" => tag3.id,
-  "amount" => 3000
+  "amount" => 30.00
 })
 transaction4.save()
 
 transaction5 = Transaction.new({
   "merchant_id" => merchant2.id,
   "tag_id" => tag7.id,
-  "amount" => 499
+  "amount" => 4.99
 })
 transaction5.save()
 
 transaction6 = Transaction.new({
   "merchant_id" => merchant2.id,
   "tag_id" => tag5.id,
-  "amount" => 14000
+  "amount" => 140.00
 })
 transaction6.save()
 
--- models/transactions.rb
+++ models/transactions.rb
@@ -1,5 +1,4 @@
 require_relative('../db/sqlrunner.rb')
-require('money')
 
 class Transaction
 
@@ -9,7 +8,7 @@ class Transaction
     @id = options['id'].to_i if options['id']
     @merchant_id = options['merchant_id'].to_i
     @tag_id = options['tag_id'].to_i
-    @amount = options['amount'].cents
+    @amount = options['amount'].to_i
   end
 
   def pretty_amount()
