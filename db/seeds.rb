310877a48f7c7953220289d1f67b457a512f126f
Gregor Cox <gregorcox@gmail.com>
2018-06-20 10:52:48 +0100

    adds dates to transactions

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ require_relative('../models/merchants')
 require_relative('../models/tags')
 require_relative('../models/transactions')
 require('pry')
+require('date')
 
 Transaction.delete_all()
 Tag.delete_all()
@@ -75,42 +76,48 @@ merchant5.save()
 transaction1 = Transaction.new({
   "merchant_id" => merchant1.id,
   "tag_id" => tag1.id,
-  "amount" => 2452
+  "amount" => 2452,
+  "timestamp" => Date.strptime('03-02-2001', '%d-%m-%Y')
 })
 transaction1.save()
 
 transaction2 = Transaction.new({
   "merchant_id" => merchant3.id,
   "tag_id" => tag5.id,
-  "amount" => 1200
+  "amount" => 1200,
+  "timestamp" => Date.strptime('03-02-2001', '%d-%m-%Y')
 })
 transaction2.save()
 
 transaction3 = Transaction.new({
   "merchant_id" => merchant4.id,
   "tag_id" => tag6.id,
-  "amount" => 799
+  "amount" => 799,
+  "timestamp" => Date.strptime('03-02-2001', '%d-%m-%Y')
 })
 transaction3.save()
 
 transaction4 = Transaction.new({
   "merchant_id" => merchant5.id,
   "tag_id" => tag3.id,
-  "amount" => 3000
+  "amount" => 3000,
+  "timestamp" => Date.strptime('03-02-2001', '%d-%m-%Y')
 })
 transaction4.save()
 
 transaction5 = Transaction.new({
   "merchant_id" => merchant2.id,
   "tag_id" => tag7.id,
-  "amount" => 499
+  "amount" => 499,
+  "timestamp" => Date.strptime('03-02-2001', '%d-%m-%Y')
 })
 transaction5.save()
 
 transaction6 = Transaction.new({
   "merchant_id" => merchant2.id,
   "tag_id" => tag5.id,
-  "amount" => 14000
+  "amount" => 14000,
+  "timestamp" => Date.strptime('03-02-2001', '%d-%m-%Y')  
 })
 transaction6.save()
 
