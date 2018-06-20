a2ff479e0f20c9a77e2851db1d53c22cc28e1a51
Gregor Cox <gregorcox@gmail.com>
2018-06-20 21:00:30 +0100

    adds additional transactions

--- db/seeds.rb
+++ db/seeds.rb
@@ -83,11 +83,21 @@ merchant5 = Merchant.new({
 })
 merchant5.save()
 
+merchant6 = Merchant.new({
+  "name" => "Nike"
+})
+merchant6.save()
+
+merchant7 = Merchant.new({
+  "name" => "British Gas"
+})
+merchant7.save()
+
 transaction1 = Transaction.new({
   "merchant_id" => merchant1.id,
   "tag_id" => tag1.id,
   "amount" => 2452,
-  "timestamp" => Date.strptime('03-02-2001', '%d-%m-%Y')
+  "timestamp" => Date.strptime('05-06-2018', '%d-%m-%Y')
 })
 transaction1.save()
 
@@ -95,7 +105,7 @@ transaction2 = Transaction.new({
   "merchant_id" => merchant3.id,
   "tag_id" => tag5.id,
   "amount" => 1200,
-  "timestamp" => Date.strptime('03-02-2001', '%d-%m-%Y')
+  "timestamp" => Date.strptime('18-05-2018', '%d-%m-%Y')
 })
 transaction2.save()
 
@@ -103,7 +113,7 @@ transaction3 = Transaction.new({
   "merchant_id" => merchant4.id,
   "tag_id" => tag6.id,
   "amount" => 799,
-  "timestamp" => Date.strptime('03-02-2001', '%d-%m-%Y')
+  "timestamp" => Date.strptime('29-05-2018', '%d-%m-%Y')
 })
 transaction3.save()
 
@@ -111,7 +121,7 @@ transaction4 = Transaction.new({
   "merchant_id" => merchant5.id,
   "tag_id" => tag3.id,
   "amount" => 3000,
-  "timestamp" => Date.strptime('03-02-2001', '%d-%m-%Y')
+  "timestamp" => Date.strptime('04-06-2001', '%d-%m-%Y')
 })
 transaction4.save()
 
@@ -119,7 +129,7 @@ transaction5 = Transaction.new({
   "merchant_id" => merchant2.id,
   "tag_id" => tag7.id,
   "amount" => 499,
-  "timestamp" => Date.strptime('03-02-2001', '%d-%m-%Y')
+  "timestamp" => Date.strptime('28-12-2017', '%d-%m-%Y')
 })
 transaction5.save()
 
@@ -127,10 +137,25 @@ transaction6 = Transaction.new({
   "merchant_id" => merchant2.id,
   "tag_id" => tag5.id,
   "amount" => 14000,
-  "timestamp" => Date.strptime('03-02-2001', '%d-%m-%Y')
+  "timestamp" => Date.strptime('23-01-2018', '%d-%m-%Y')
 })
 transaction6.save()
 
+transaction7 = Transaction.new({
+  "merchant_id" => merchant6.id,
+  "tag_id" => tag10.id,
+  "amount" => 1495,
+  "timestamp" => Date.strptime('20-05-2018', '%d-%m-%Y')
+})
+transaction7.save()
+
+transaction8 = Transaction.new({
+  "merchant_id" => merchant7.id,
+  "tag_id" => tag2.id,
+  "amount" => 8563,
+  "timestamp" => Date.strptime('05-05-2018', '%d-%m-%Y')
+})
+transaction8.save()
 
 binding.pry
 nil
